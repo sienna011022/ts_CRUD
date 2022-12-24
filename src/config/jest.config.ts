7134fe7526785dec08b00c539fed6fe33085a629
sienna <20200803@sungshin.ac.kr>
2022-12-24 11:16:03 +0900

    :hammer:chroe : config move & create

--- src/config/jest.config.ts
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: "ts-jest", 
-  testEnvironment: "node", 
-  testMatch:  ["<rootDir>/**/*.test.(js|jsx|ts|tsx)",
-  "<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))",
-]};
