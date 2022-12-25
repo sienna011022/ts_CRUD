9b913bba92f3118fd87cea0802a101a0f054fb70
sienna <20200803@sungshin.ac.kr>
2022-12-25 13:35:29 +0900

    :recycle:refactor : error response 수정

--- src/controller/ArticleController.ts
+++ src/controller/ArticleController.ts
@@ -2,10 +2,16 @@ import { Request, Response } from "express";
 import createArticleDto from "../service/dto/CreateArticleDto";
 import articleService from "../service/ArticleService";
 
-export  class ArticleController {
+export class ArticleController {
   async createArticle(request: Request, response: Response) {
-    const article = createArticleDto.newArticleDto(request);
-    await articleService.createArticle(article);
+    try {
+      const article = createArticleDto.newArticleDto(request);
+      await articleService.createArticle(article);
+    } catch (exception) {
+      response.status(400).json({
+        errortype : exception.message 
+      });
+    }
     response.status(201).json();
   }
 }
