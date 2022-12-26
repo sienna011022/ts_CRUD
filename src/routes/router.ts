22c0495af98817c5c224c300e2fe65433e232cf7
sienna <20200803@sungshin.ac.kr>
2022-12-26 20:42:18 +0900

    :recycle:refactor : 공백 제거

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,6 +1,5 @@
-import { ArticleController } from "../controller/ArticleController";
 import express from "express";
-
+import { ArticleController } from "../controller/ArticleController";
 import { UserController } from "../controller/UserController";
 
 const router = express.Router();
@@ -12,4 +11,5 @@ router.get("/users", userController.findUsers);
 
 router.post("/users/:user_id/articles", articleController.createArticle);
 router.get("/users/:user_id/articles", articleController.findAllArticle);
+
 export default router;
--- test/src/service/dto/CreateUserDto.test.ts
+++ test/src/service/dto/CreateUserDto.test.ts
@@ -1,22 +1,24 @@
 import { User } from "../../../../src/entity/User";
 import CreateUserDto from "../../../../src/service/dto/CreateUserDto";
 
-test("CreateUserDto 객체를 반환한다", () => {
-  const request = {
-    userId: "sienna",
-    password: "1234",
-    email: "sienna011022@naver.com",
-  };
-  expect(new CreateUserDto(request)).toBeInstanceOf(CreateUserDto);
-});
+describe("CreateUserDto", () => {
+  it("CreateUserDto 객체를 반환한다", () => {
+    const request = {
+      userId: "sienna",
+      password: "1234",
+      email: "sienna011022@naver.com",
+    };
+    expect(new CreateUserDto(request)).toBeInstanceOf(CreateUserDto);
+  });
 
-test("CreateUserDto 를 User로 매핑한다", () => {
-  const request = {
-    userId: "sienna",
-    password: "1234",
-    email: "sienna011022@naver.com",
-  };
-  const requestDto = new CreateUserDto(request);
+  it("CreateUserDto.createUser()는 User를 반환한다", () => {
+    const request = {
+      userId: "sienna",
+      password: "1234",
+      email: "sienna011022@naver.com",
+    };
+    const requestDto = new CreateUserDto(request);
 
-  expect(requestDto.createUser()).toBeInstanceOf(User);
+    expect(requestDto.createUser()).toBeInstanceOf(User);
+  });
 });
